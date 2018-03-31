922d22f45b24c32388a7fb67301fd452855aaa41
Midhun Gundapuneni <g.midhun@columbia.edu>
2018-03-31 18:49:29 -0400

    version changes

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,23 +1,22 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var parentDir = path.join(__dirname, '../');
+var parentDir = path.join(__dirname, '/');
 
 module.exports = {
     entry: [
         path.join(parentDir, 'index.js')
     ],
     module: {
-        loaders: [{
-            test: /\.(js|jsx)$/,
+        rules: [{
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-					presets: ['react','es2015','stage-0']
-				}
+                user: {
+                    loader: 'babel-loader'
+                }
             },{
                 test: /\.less$/,
-                loaders: ["style-loader", "css-loader", "less-loader"]
+                use: ["style-loader", "css-loader", "less-loader"]
             }
         ]
     },
