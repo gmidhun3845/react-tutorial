aa483ad5fa4a0022b72de8b05ae97f54e60ee914
Midhun Gundapuneni <g.midhun@columbia.edu>
2018-03-31 20:49:10 -0400

    change name

--- containers/App.js
+++ containers/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import Rectangle from '../components/rectangle'
 
+const Rectangle = () => (
+    <rect width="100" height="200" x="50" y="20" />
+);
+
 export default class App extends Component {
     render () {
         return (
         	<div>
-        		<h1>This is my new react app</h1>
+        		<h1 style={{color:"green"}}>React + D3</h1>
         		<svg><Rectangle width="30" height="100" x="30" y="30"></Rectangle></svg>
         	</div>
         );
