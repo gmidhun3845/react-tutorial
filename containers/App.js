30a56b3806e5c99d93b0bf1c687020bd5395f180
chinwen <chinwen@dyn-160-39-235-216.dyn.columbia.edu>
2018-03-31 20:44:38 -0400

    rect

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
+import Rectangle from '../components/rectangle'
 
 export default class App extends Component {
     render () {
-        return <p>This is my new react app</p>
+        return (
+        	<div>
+        		<h1>This is my new react app</h1>
+        		<svg><Rectangle width="30" height="100" x="30" y="30"></Rectangle></svg>
+        	</div>
+        );
     }
 }
